ed35d75d106f760c6595e3b84854600e01fe4500
Eliabe G da Paz <eliabepaz.work@gmail.com>
2025-11-03 19:30:06 -0300

    design

--- app/c_admin/classes/conteudo.painel-user.class.php
+++ app/c_admin/classes/conteudo.painel-user.class.php
@@ -20,7 +20,7 @@ class ConteudoPainelUser
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Gestão de Usuários</title>
-                <link rel="stylesheet" href="../src/style.css">
+                <link rel="stylesheet" href="src/style.css">
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
             </head>
         HTML;
