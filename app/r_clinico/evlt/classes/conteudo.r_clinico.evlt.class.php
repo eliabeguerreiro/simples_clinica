8767a6d3ad3979750cb2b0974c54461bcc1ea83a
Eliabe G da Paz <eliabepaz.work@gmail.com>
2026-01-23 22:58:22 -0300

    UPDATE EVLT
    ajuste de nav

--- app/r_clinico/evlt/classes/conteudo.r_clinico.evlt.class.php
+++ app/r_clinico/evlt/classes/conteudo.r_clinico.evlt.class.php
@@ -139,8 +139,9 @@ class ConteudoRClinicoEvlt
                     </div>
                     <nav>
                         <ul>
-                            <li><a href="../../">INICIO</a></li>
-                            <li><a href="#">SUPORTE</a></li>
+                            <!-- Ícone de casa: volta ao menu principal -->
+                            <li><a href="../../" title="Voltar ao Menu Principal"><i class="fas fa-home"></i></a></li>
+                            <!-- Informações do usuário -->
                             <li class="user-info">
                                 <span class="user-icon"><i class="fas fa-user"></i></span>
                                 <div class="user-details">
